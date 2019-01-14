8aa0f05ac614cba4906d0899622f5878f8b718b2
simon <simon.ilic@student.uva.nl>
2019-01-14 15:02:03 +0100

    created circumbinary planetairy system simulation

---
+++ circumbinary.py
@@ -0,0 +1,11 @@
+import rebound
+
+sim = rebound.Simulation() #initiate simulation
+sim.add(m=1.0) #Star A
+sim.add(m=1.0, a=1.0) # Star B
+# add circumbinary planet ABb
+sim.add(a=2.0)
+
+fig = rebound.OrbitPlot(sim)
+fig.savefig("circumbinary.png") # save figure to file
+fig.show() # show fipigure on screen
