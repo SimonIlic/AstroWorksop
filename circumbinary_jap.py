0213e2ba5cc8f3c856c4429b453a78958b149aae
jasperlankhorst <lankhorstjasper@gmail.com>
2019-01-16 10:55:00 +0100

    Kleine fix

--- circumbinary_jap.py
+++ circumbinary_jap.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 sim = rebound.Simulation()
 sim.add(["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn",
-         "Uranus", "Neptune"], date="99999-01-01 06:25")
+         "Uranus", "Neptune"], date="9999-01-01 06:25")
 
 fig = rebound.OrbitPlot(sim)
 fig.savefig("circumbinary_j.png")
