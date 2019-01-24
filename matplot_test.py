1e94e3ced2c8a68e4fe1d3c11f26e359614024ce
jasperlankhorst <lankhorstjasper@gmail.com>
2019-01-24 10:14:20 +0100

    Plaatjes verwijderd

--- matplot_test.py
+++ matplot_test.py
@@ -1,3 +1,4 @@
+# matplot_test.py
 # Jasper Lankhorst
 # Program to make a gif of a simulation of planets
 
@@ -17,15 +18,19 @@ sim.integrate(sim.t+600)
 # center plot at sun
 sim.move_to_hel()
 
-# make r plots
+# make r plots, append to list to create gif
 r = 3
 images = np.array([])
 for i in range(r):
     sim.integrate(sim.t+10)
     fig = rebound.OrbitPlot(sim,color=True,unitlabel="[AU]", lim=250.,
         show_orbit=True)
-    print ("%s of 300." %i)
-    plt.savefig("plotje_%s.png" %i)
-    np.append(images, "plotje_%s.png" %i)
+    print(f"{i + 1} of {r}.")
+    plt.savefig(f"/plaatjes/plotje_{i}.png")
+    images = np.append(images, [imageio.imread(f"/plaatjes/plotje_{i}.png")])
+    print((images[0]))
+    if isinstance(images, np.ndarray):
+        print(f"Dit is een fucking numpy array maat, check: {type(images)}")
     plt.close('all')
+
 imageio.mimsave('fly_by_gifje.gif', images)
